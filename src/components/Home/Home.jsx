08f421831f6412f498c46c814cdd76e3f6f26a57
Fabian Ramirez <fabioerv_1994@hotmail.com>
2023-09-30 11:33:00 -0500

    commit otro pc

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className={styles.divTexto}>
                 <h1>Hey, soy Fabián Ramírez</h1>
                 <h2 style={{color: '#49b4b2'}}>Full Stack Developer</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae similique 
+                <p>Desarrolador Full Satck, Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae similique 
                     neque assumenda doloremque dolore iure consectetur ducimus corporis illum 
                     labore architecto alias, cumque illo odio nulla at. Autem, quas doloribus.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae similique 
